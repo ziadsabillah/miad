8a940e0630069e22d3eb078de8c27164993e8ba6
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-07-02 16:41:42 +0100

    verifying apollo server working + added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import 'dotenv/config';
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
 
 
 const app = express();
+
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Ziad Sabillah'
+      }
+    }
+  }
+}
+
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql'});
+
+app.listen({port: 8000}, () => {
+  console.log('Apollo Server started on http://localhost:8000/graphql');
+});
+
 
 const userCredentials = { firstname: 'Robin' };
 const userDetails = { nationality: 'German' };
