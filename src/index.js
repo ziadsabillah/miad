ebdbbf06cd1550cfa6e4d15e49008fc4e1c9179d
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-07-03 03:49:19 +0100

    technical seperation and experimenting with apollo playground

--- src/index.js
+++ src/index.js
@@ -3,57 +3,24 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import cors from 'cors';
 
+import { v4 as uuidv4 } from 'uuid';
+
+import schema from './schema'
+import models from './models'
+import resolvers from './resolvers'
+
 
 const app = express();
 
 app.use(cors());
 
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Ziad Sabillah',
-  },
-  2: {
-    id: '2',
-    username: 'David Davids'
-  }
-}
-
-const me = users[1];
-
-
-const schema = gql`
-  type Query {
-    users: [User!]
-    me: User
-    user(id: ID!): User
-  }
-
-  type User {
-    id: ID!
-    username: String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    users: () => {
-      return Object.values(users)
-    },
-    user: (parent, { id }) => {
-      return users[id]
-    },
-    me: () => {
-      return me;
-    }
-  }
-}
-
-
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
+  context: {
+    models,
+    me: models.users[1],
+  }
 });
 
 server.applyMiddleware({ app, path: '/graphql'});
@@ -61,16 +28,3 @@ server.applyMiddleware({ app, path: '/graphql'});
 app.listen({port: 8000}, () => {
   console.log('Apollo Server started on http://localhost:8000/graphql');
 });
-
-
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
-
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
-
-console.log(user);
-
-console.log(process.env.SOME_ENV_VARIABLE);
